c9e3c7677f7cb1d505b9d3be536f8aec81e50389
guoliang <648100535@qq.com>
2024-05-22 21:26:51 +0800

    前端代码格式化

--- resources/js/smoothscroll.js
+++ resources/js/smoothscroll.js
@@ -15,8 +15,8 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
  * @author Ariel Flesler
  * @version 1.4.3.1
  */
-; (function($) {
-    var h = $.scrollTo = function(a, b, c) {
+; (function ($) {
+    var h = $.scrollTo = function (a, b, c) {
         $(window).scrollTo(a, b, c)
     };
     h.defaults = {
@@ -24,19 +24,19 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
         duration: parseFloat($.fn.jquery) >= 1.3 ? 0 : 1,
         limit: true
     };
-    h.window = function(a) {
+    h.window = function (a) {
         return $(window)._scrollable()
     };
-    $.fn._scrollable = function() {
-        return this.map(function() {
+    $.fn._scrollable = function () {
+        return this.map(function () {
             var a = this,
-            isWin = !a.nodeName || $.inArray(a.nodeName.toLowerCase(), ['iframe', '#document', 'html', 'body']) != -1;
+                isWin = !a.nodeName || $.inArray(a.nodeName.toLowerCase(), ['iframe', '#document', 'html', 'body']) != -1;
             if (!isWin) return a;
             var b = (a.contentWindow || a).document || a.ownerDocument || a;
-            return /webkit/i.test(navigator.userAgent) || b.compatMode == 'BackCompat' ? b.body: b.documentElement
+            return /webkit/i.test(navigator.userAgent) || b.compatMode == 'BackCompat' ? b.body : b.documentElement
         })
     };
-    $.fn.scrollTo = function(e, f, g) {
+    $.fn.scrollTo = function (e, f, g) {
         if (typeof f == 'object') {
             g = f;
             f = 0
@@ -46,77 +46,77 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
         };
         if (e == 'max') e = 9e9;
         g = $.extend({},
-        h.defaults, g);
+            h.defaults, g);
         f = f || g.duration;
         g.queue = g.queue && g.axis.length > 1;
         if (g.queue) f /= 2;
         g.offset = both(g.offset);
         g.over = both(g.over);
-        return this._scrollable().each(function() {
+        return this._scrollable().each(function () {
             if (e == null) return;
             var d = this,
-            $elem = $(d),
-            targ = e,
-            toff,
-            attr = {},
-            win = $elem.is('html,body');
+                $elem = $(d),
+                targ = e,
+                toff,
+                attr = {},
+                win = $elem.is('html,body');
             switch (typeof targ) {
-            case 'number':
-            case 'string':
-                if (/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)) {
-                    targ = both(targ);
-                    break
-                }
-                targ = $(targ, this);
-                if (!targ.length) return;
-            case 'object':
-                if (targ.is || targ.style) toff = (targ = $(targ)).offset()
+                case 'number':
+                case 'string':
+                    if (/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)) {
+                        targ = both(targ);
+                        break
+                    }
+                    targ = $(targ, this);
+                    if (!targ.length) return;
+                case 'object':
+                    if (targ.is || targ.style) toff = (targ = $(targ)).offset()
             }
             $.each(g.axis.split(''),
-            function(i, a) {
-                var b = a == 'x' ? 'Left': 'Top',
-                pos = b.toLowerCase(),
-                key = 'scroll' + b,
-                old = d[key],
-                max = h.max(d, a);
-                if (toff) {
-                    attr[key] = toff[pos] + (win ? 0 : old - $elem.offset()[pos]);
-                    if (g.margin) {
-                        attr[key] -= parseInt(targ.css('margin' + b)) || 0;
-                        attr[key] -= parseInt(targ.css('border' + b + 'Width')) || 0
+                function (i, a) {
+                    var b = a == 'x' ? 'Left' : 'Top',
+                        pos = b.toLowerCase(),
+                        key = 'scroll' + b,
+                        old = d[key],
+                        max = h.max(d, a);
+                    if (toff) {
+                        attr[key] = toff[pos] + (win ? 0 : old - $elem.offset()[pos]);
+                        if (g.margin) {
+                            attr[key] -= parseInt(targ.css('margin' + b)) || 0;
+                            attr[key] -= parseInt(targ.css('border' + b + 'Width')) || 0
+                        }
+                        attr[key] += g.offset[pos] || 0;
+                        if (g.over[pos]) attr[key] += targ[a == 'x' ? 'width' : 'height']() * g.over[pos]
+                    } else {
+                        var c = targ[pos];
+                        attr[key] = c.slice && c.slice(- 1) == '%' ? parseFloat(c) / 100 * max : c
                     }
-                    attr[key] += g.offset[pos] || 0;
-                    if (g.over[pos]) attr[key] += targ[a == 'x' ? 'width': 'height']() * g.over[pos]
-                } else {
-                    var c = targ[pos];
-                    attr[key] = c.slice && c.slice( - 1) == '%' ? parseFloat(c) / 100 * max: c
-                }
-                if (g.limit && /^\d+$/.test(attr[key])) attr[key] = attr[key] <= 0 ? 0 : Math.min(attr[key], max);
-                if (!i && g.queue) {
-                    if (old != attr[key]) animate(g.onAfterFirst);
-                    delete attr[key]
-                }
-            });
+                    if (g.limit && /^\d+$/.test(attr[key])) attr[key] = attr[key] <= 0 ? 0 : Math.min(attr[key], max);
+                    if (!i && g.queue) {
+                        if (old != attr[key]) animate(g.onAfterFirst);
+                        delete attr[key]
+                    }
+                });
             animate(g.onAfter);
             function animate(a) {
                 $elem.animate(attr, f, g.easing, a &&
-                function() {
-                    a.call(this, e, g)
-                })
+                    function () {
+                        a.call(this, e, g)
+                    })
             }
         }).end()
     };
-    h.max = function(a, b) {
-        var c = b == 'x' ? 'Width': 'Height',
-        scroll = 'scroll' + c;
+    h.max = function (a, b) {
+        var c = b == 'x' ? 'Width' : 'Height',
+            scroll = 'scroll' + c;
         if (!$(a).is('html,body')) return a[scroll] - $(a)[c.toLowerCase()]();
         var d = 'client' + c,
-        html = a.ownerDocument.documentElement,
-        body = a.ownerDocument.body;
+            html = a.ownerDocument.documentElement,
+            body = a.ownerDocument.body;
         return Math.max(html[scroll], body[scroll]) - Math.min(html[d], body[d])
     };
     function both(a) {
-        return typeof a == 'object' ? a: {
+        return typeof a == 'object' ? a : {
             top: a,
             left: a
         }
@@ -131,22 +131,22 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
  * @author Ariel Flesler
  * @version 1.2.8b
  **/
-; (function(b) {
+; (function (b) {
     function g(a, e, d) {
         var h = e.hash.slice(1),
-        f = document.getElementById(h) || document.getElementsByName(h)[0];
+            f = document.getElementById(h) || document.getElementsByName(h)[0];
         if (f) {
             a && a.preventDefault();
             var c = b(d.target);
-            if (! (d.lock && c.is(":animated") || d.onBefore && !1 === d.onBefore(a, f, c))) {
+            if (!(d.lock && c.is(":animated") || d.onBefore && !1 === d.onBefore(a, f, c))) {
                 d.stop && c._scrollable().stop(!0);
                 if (d.hash) {
-                    var a = f.id == h ? "id": "name",
-                    g = b("<a> </a>").attr(a, h).css({
-                        position: "absolute",
-                        top: b(window).scrollTop(),
-                        left: b(window).scrollLeft()
-                    });
+                    var a = f.id == h ? "id" : "name",
+                        g = b("<a> </a>").attr(a, h).css({
+                            position: "absolute",
+                            top: b(window).scrollTop(),
+                            left: b(window).scrollLeft()
+                        });
                     f[a] = "";
                     b("body").prepend(g);
                     location = e.hash;
@@ -158,9 +158,9 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
         }
     }
     var i = location.href.replace(/#.*/, ""),
-    c = b.localScroll = function(a) {
-        b("body").localScroll(a)
-    };
+        c = b.localScroll = function (a) {
+            b("body").localScroll(a)
+        };
     c.defaults = {
         duration: 1E3,
         axis: "y",
@@ -169,10 +169,10 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
         target: window,
         reset: !0
     };
-    c.hash = function(a) {
+    c.hash = function (a) {
         if (location.hash) {
             a = b.extend({},
-            c.defaults, a);
+                c.defaults, a);
             a.hash = !1;
             if (a.reset) {
                 var e = a.duration;
@@ -183,25 +183,25 @@ if (!window['jQuery']) alert('The jQuery library must be included before the smo
             g(0, location, a)
         }
     };
-    b.fn.localScroll = function(a) {
+    b.fn.localScroll = function (a) {
         function e() {
-            return !! this.href && !!this.hash && this.href.replace(this.hash, "") == i && (!a.filter || b(this).is(a.filter))
+            return !!this.href && !!this.hash && this.href.replace(this.hash, "") == i && (!a.filter || b(this).is(a.filter))
         }
         a = b.extend({},
-        c.defaults, a);
+            c.defaults, a);
         return a.lazy ? this.bind(a.event,
-        function(d) {
-            var c = b([d.target, d.target.parentNode]).filter(e)[0];
-            c && g(d, c, a)
-        }) : this.find("a,area").filter(e).bind(a.event,
-        function(b) {
-            g(b, this, a)
-        }).end().end()
+            function (d) {
+                var c = b([d.target, d.target.parentNode]).filter(e)[0];
+                c && g(d, c, a)
+            }) : this.find("a,area").filter(e).bind(a.event,
+                function (b) {
+                    g(b, this, a)
+                }).end().end()
     }
 })(jQuery);
 
 // Initialize all .smoothScroll links
-jQuery(function($) {
+jQuery(function ($) {
     $.localScroll({
         filter: '.smoothScroll'
     });
